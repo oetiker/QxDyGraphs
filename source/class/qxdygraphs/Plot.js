2126f89fc15fc14d3a6abbe53f47fef5ad0118c1
Tobias Oetiker <tobi@oetiker.ch>
2011-03-06 19:33:42 +0000

    its labels not lables

--- source/class/qxdygraphs/Plot.js
+++ source/class/qxdygraphs/Plot.js
@@ -34,7 +34,7 @@
  *     data.push([new Date(1000000000+i*3600*1000),Math.random(),Math.sin(i/50)]);
  * }
  * var options = {
- *    lables: [ 'Random','Sin' ]
+ *    labels: [ 'Random','Sin' ]
  * };
  * var plot = new qxdygraphs.Plot(data,options); 
  * </pre>
