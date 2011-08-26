69ad2372caa00ee1f2fb908e598683c2958ab180
oetiker <oetiker@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2011-08-26 07:46:20 +0000

    fork to make way for 1.5 update

---
+++ trunk/qooxdoo-contrib/QxDyGraphs/0.1/trunk/source/class/qxdygraphs/__init__.js
@@ -0,0 +1,9 @@
+/** <h3> qxdygraphs API Documentation </h3>
+ *
+ * dygraphs is an open source JavaScript library that produces produces
+ * interactive, zoomable charts of time series. It is designed to display
+ * dense data sets and enable users to explore and interpret them.
+ *
+ * QxDyGraphs provides a qooxdoo wrapper for this library.
+ *
+ */
